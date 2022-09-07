03c42f275798bdc7f1dc8901fb5cf38271e8d49f
alrasheedftah <alrasheedrasta@gmail.com>
2022-09-08 02:37:09 +0400

    add Interfaces To  ERC721Metadata

--- contracts/ERC721Metadata.sol
+++ contracts/ERC721Metadata.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
+import "./interfaces/IERC721Metadata.sol";
 
-contract ERC721Metadata {
+contract ERC721Metadata is IERC721Metadata {
     string private _name ;
 
     string private _symbol;
@@ -12,11 +13,11 @@ contract ERC721Metadata {
         _symbol = newSymbol; 
     }
 
-    function name() external view returns (string memory){
+    function name() external view  override returns (string memory){
         return _name;
     }
 
-    function symbol() external view returns (string memory){
+    function symbol() external view override returns (string memory){
         return _symbol;
     }    
 } 
---
+++ contracts/interfaces/IERC721Metadata.sol
@@ -0,0 +1,12 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+interface IERC721Metadata {
+
+    function name() external view returns (string memory);
+
+    function symbol() external  view returns (string memory);
+
+
+}
